6e0bf12fec980278d3732b3f548fb57db7e08506
xiaoj <838249367@qq.com>
2018-11-09 17:57:57 +0800

    add 502

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -37,7 +37,8 @@ module.exports = {
           title: '杂记',
           collapsable: false,
           children:[
-            '501'
+            '501',
+            '502'
           ]
         }
           ],
